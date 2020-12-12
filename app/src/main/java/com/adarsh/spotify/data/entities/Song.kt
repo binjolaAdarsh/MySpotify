dc0ecf880cb15c97372f140b18ad5a6c49cc1fe4
adarshbinjola <adarsh.binjola@byjus.com>
2020-12-12 16:40:26 +0530

    added song class

---
+++ app/src/main/java/com/adarsh/spotify/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.adarsh.spotify.data.entities
+
+data class Song(
+    val mediaId: Int = 0,
+    val title: String = "",
+    val subTitle: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+)
